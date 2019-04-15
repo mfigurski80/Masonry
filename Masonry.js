fe3f1408cd5b68b818d7003a23aa1dad1963a950
Mikolaj Figurski <meek.f80@gmail.com>
2019-04-15 13:20:28 -0400

    Added comments, moved stuff around

--- Masonry.js
+++ Masonry.js
@@ -1,67 +1,92 @@
-const Tile = require('./Tile.js');
-
-/* Larger class to build and organize Tiles
-*/
-module.exports = class Masonry {
+/**
+ * Container class for Tile, distributes and organizes
+ * @param {HTML Element} elem Container within which to place tiles
+ * @param {String} distribution Mode of distributing tiles (center, top, left)
+ * @param {Int} resolution How small the tile areas are gonna be
+ */
+class Masonry {
     constructor(elem, distribution = "center", resolution = 32) {
         this.elem = elem;
         this.maxWidth = resolution;
         const boxSize = this.elem.offsetWidth / this.maxWidth;
         this.maxHeight = Math.floor(this.elem.offsetHeight / boxSize);
         this.centerSpace = [Math.floor(this.maxWidth / 2), Math.floor(this.maxHeight / 2)];
-        this.elem.style.setProperty("display", "grid");
+        this.elem.style.setProperty("display", "grid"); // tiles will be distributed on Grid... set it
         this.elem.style.setProperty("grid-auto-columns", boxSize + "px");
         this.elem.style.setProperty("grid-auto-rows", boxSize + "px");
         this.distribution = distribution;
         this.placedTiles = [];
-        this.possibleDim = [
+        this.possibleDim = [ // initial possible tile dimensions
             [2, 2],
             [2, 3],
             [3, 2]
         ]
     }
+
+    /**
+     * Checks for overlap in position between two given Tile objects
+     * @param  {Tile} a Tile A
+     * @param  {Tile} b Tile B
+     * @return {Bool} Whether or not they overlap
+     */
     areTilesConflicting(a, b) {
         if ((a.x <= b.x && b.x < a.btmX) || (a.x < b.btmX && b.btmX <= a.btmX) || (b.x <= a.x && a.x < b.btmX) || (b.x < a.btmX && a.btmX <= b.btmX)) {
             if ((a.y <= b.y && b.y < a.btmY) || (a.y < b.btmY && b.btmY <= a.btmY) || (b.y <= a.y && a.y < b.btmY) || (b.y < a.btmY && a.btmY <= b.btmY)) {
-                return !0
+                return true;
             }
         }
-        return !1
+        return false;
     }
+
+    /**
+     * Check if Tile doesn't conflict with any other placed Tiles and if doesn't overflow
+     * @param  {Tile} tile Tile to check pos of
+     * @return {Boolean} Is Tile position okay?
+     */
     isTilePosAcceptable(tile) {
         for (var placedTile of this.placedTiles) {
-            if (this.areTilesConflicting(tile, placedTile)) {
-                return (!1)
-            }
+            if (this.areTilesConflicting(tile, placedTile)) return false;
         };
         if (tile.x < 1 || tile.y < 1 || (tile.btmX - 1 > this.maxWidth && this.distribution != "left") || (tile.btmY - 1 > this.maxHeight && this.distribution != "top")) {
-            return !1
+            return false;
         }
-        return !0
+        return true;
     }
+    /**
+     * Grabs random dimensions and returns Tile object of that size
+     * @return {Tile} Tile of random dimension
+     */
     getRandSizeTile() {
         const rand = Math.floor(Math.random() * this.possibleDim.length);
         return (new Tile(1, 1, this.possibleDim[rand][0], this.possibleDim[rand][1]))
     }
+    /**
+     * Shift tile around in given space to make it fit
+     * @param  {Tile} tile Tile to fit
+     * @param  {Array} space Location of space to fit in
+     * @return {Boolean} Whether has been fitted
+     */
     isTileFit(tile, space) {
         tile.setXY(space[0], space[1]);
-        if (this.isTilePosAcceptable(tile)) {
-            return !0
-        }
+        if (this.isTilePosAcceptable(tile)) return true;
+
         tile.shiftX();
-        if (this.isTilePosAcceptable(tile)) {
-            return !0
-        }
+        if (this.isTilePosAcceptable(tile)) return true;
+
         tile.shiftY();
-        if (this.isTilePosAcceptable(tile)) {
-            return !0
-        }
+        if (this.isTilePosAcceptable(tile)) return true;
+
         tile.unshiftX();
-        if (this.isTilePosAcceptable(tile)) {
-            return !0
-        }
-        return !1
+        if (this.isTilePosAcceptable(tile)) return true;
+
+        return false;
     }
+
+    /**
+     * Find space to check next, based on distribution mode
+     * @param  {Array} oldSpaces Last used space
+     * @return {Array} New space
+     */
     getNextSpaceToCheck(oldSpaces) {
         var newSpaces = [];
         if (this.distribution == "center") {
@@ -115,6 +140,10 @@ module.exports = class Masonry {
         });
         return retSpaces
     }
+    /**
+     * Grab elements with given Class names, form tiles for them, and render them out
+     * @param  {String} className Class identifier for Tile elements
+     */
     placeTilesByClass(className) {
         var tileElems = Array.from(document.getElementsByClassName(className));
         var tiles = [];
@@ -132,6 +161,13 @@ module.exports = class Masonry {
         });
         this.placeTiles(tiles)
     }
+    /**
+     * Generate new elements as Tiles and place them
+     * @param  {Array} classNames List of Class names to give to each elem
+     * @param  {String} styles Value of inline style of each elem
+     * @param  {String} innerHTML Value of HTML to place into each elem
+     * @return {Bool} Whether all tiles were successfully placed
+     */
     placeGeneratedTile(classNames = [], styles = "", innerHTML = "") {
         let tileElem = document.createElement("DIV");
         classNames.forEach(name => {
@@ -141,12 +177,17 @@ module.exports = class Masonry {
         var tile = app.getRandSizeTile();
         tile.setElem(tileElem);
         if (!app.placeTiles([tile])) {
-            return !1
+            return false;
         }
         tileElem.setAttribute("style", (tileElem.getAttribute("style") + " " + styles));
         this.elem.appendChild(tileElem);
-        return !0
+        return true;
     }
+    /**
+     * Fits given Tiles into current Masonry and renders/places them there
+     * @param  {Array} newTiles Array of Tile objects
+     * @return {Bool} Whether all tiles weere successfully placed
+     */
     placeTiles(newTiles) {
         var curSpaces = [];
         while (newTiles.length > 0) {
@@ -168,6 +209,10 @@ module.exports = class Masonry {
         }
         return this.placedTiles.length
     }
+    /**
+     * Converts Masonry layout to printable String
+     * @return {String} Masonry layout description
+     */
     toString() {
         var retString = "";
         this.placedTiles.forEach(tile => {
--- Tile.js
+++ Tile.js
@@ -1,6 +1,12 @@
-/* Holds and is able to render a Tile -- functions like React Component tbh
-*/
-module.exports = class Tile {
+/**
+ * Holds necessary information for Tile element, as well as useful Tile
+ * manipulation methods and a rendering method
+ * @param {Int} x X position
+ * @param {Int} y Y position
+ * @param {Int} height Tile height
+ * @param {Int} width Tile width
+ */
+class Tile {
     constructor(x, y, height, width) {
         this.x = x;
         this.y = y;
@@ -9,19 +15,38 @@ module.exports = class Tile {
         this.resetBtm();
         this.elem
     }
+    /**
+     * Sets elem to enable rendering
+     * @param {HTML Element} element HTML element that should be the tile
+     */
     setElem(element) {
         this.elem = element
     }
+    /**
+     * Resets position
+     * @param {int} x X Position
+     * @param {int} y Y Position
+     */
     setXY(x, y) {
         this.x = x;
         this.y = y;
         this.resetBtm()
     }
+    /**
+     * Resets height
+     * @param {int} height The height
+     * @param {width} width The width
+     */
     setDim(height, width) {
         this.height = height;
         this.width = width;
         this.resetBtm()
     }
+
+    /**
+     * ALL SHIFT AND UNSHIFT:
+     * Manipulate tile position (down, up, left, right)
+     */
     shiftX() {
         this.x = this.x - this.width + 1;
         this.resetBtm()
@@ -38,18 +63,33 @@ module.exports = class Tile {
         this.y = this.y + this.height - 1;
         this.resetBtm()
     }
+
+    /**
+     * UTILITY: Resets the bottom values for the Tiles
+     */
     resetBtm() {
         this.btmX = this.x + (this.width);
         this.btmY = this.y + (this.height)
     }
+    /**
+     * Imposes virtual structure onto real given html element
+     */
     render() {
         var x = this.x;
         var y = this.y;
         this.elem.setAttribute("style", "grid-column-start: " + x + "; grid-column-end: " + (this.btmX) + "; grid-row-start: " + (y) + "; grid-row-end: " + (this.btmY) + ";")
     }
+    /**
+     * Clones tile
+     * @return {Tile} Tile copy
+     */
     copy() {
         return new Tile(this.x, this.y, this.height, this.width)
     }
+    /**
+     * Returns string representation of tile
+     * @return {String} Displayable Tile information
+     */
     toString() {
         return ("x: " + this.x + "; y: " + this.y + ";\n height: " + this.height + "; width: " + this.width + ";\n btmX: " + this.btmX + "; btmY: " + this.btmY + ";")
     }
