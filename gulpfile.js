d11ea0869d1cdaf380616b1d7e1152356b9f8c59
mfigurski80 <meek.f80@gmail.com>
2019-06-29 12:41:23 -0400

    Added npm dev environment, set up gulp tsc compilation

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+
+
+gulp.task('compile-tsc', () => {
+  return gulp.src(['src/**/*.ts', 'src/**/*.js'])
+    .pipe(ts({
+      target: 'es2019',
+      allowJs: true
+    }))
+    .pipe(concat('Masonry.min.js'))
+    .pipe(terser())
+    .pipe(gulp.dest('./build'));
+});
